927e06cb43e7ff9af3d0a4b2040d7c0e262cca13
Dino0721 <99394983+Dino0721@users.noreply.github.com>
2025-12-10 15:12:44 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c13fptpqgvxd.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "kevin2004"
+customdb = "employee"
+custombucket = "kevinloyungkhang-employee"
+customregion = "us-east-1"
+
